d7e7e39035a18f0975289618f9d9f012fa27ddcb
Ganesh Mali <malishweta7434@gmail.com>
2025-12-30 14:37:53 +0530

    map added

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -158,8 +158,16 @@
 <section class="map-bx">
     <div class="container">
       <div class="row">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30773392.078304354!2d61.028322527582496!3d19.690576264827094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1749385277282!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</div>
+        <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.062408624982!2d73.780936!3d18.5860554!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c72b546f6e9d%3A0xd785c12dbfcfd2dd!2sProwess%20Buzz%20India%20-%20Food%20Consultant!5e0!3m2!1sen!2sin!4v20251230"
+        width="100%"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+    </div>
 </div>
 </section>
 
